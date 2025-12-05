864eb64e2681ce9010f1e81c80ea2f242c90ac6a
Process-ing <rbruno.p.o@gmail.com>
2025-12-05 14:56:51 +0000

    Create Go set

---
+++ src/server/utils/set.go
@@ -0,0 +1,24 @@
+package utils
+
+type Set[T comparable] map[T]struct{}
+
+func NewSet[T comparable](items ...T) Set[T] {
+	s := Set[T]{}
+	for _, item := range items {
+		s.Add(item)
+	}
+	return s
+}
+
+func (s Set[T]) Add(item T) {
+	s[item] = struct{}{}
+}
+
+func (s Set[T]) Remove(item T) {
+	delete(s, item)
+}
+
+func (s Set[T]) Contains(item T) bool {
+	_, exists := s[item]
+	return exists
+}
